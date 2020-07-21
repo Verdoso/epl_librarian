7f55893a9729026038f39032c44c04eb6511526a
Daniel Lopez <d.lopez@uib.es>
2020-07-21 12:48:42 +0200

    Añadimos explicitamente que no copiamos ese atributo

--- src/main/java/org/greeneyed/epl/librarian/services/MapperService.java
+++ src/main/java/org/greeneyed/epl/librarian/services/MapperService.java
@@ -3,8 +3,13 @@ package org.greeneyed.epl.librarian.services;
 import org.greeneyed.epl.librarian.model.Libro;
 import org.greeneyed.epl.librarian.services.EplCSVProcessor.LibroCSV;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface MapperService {
-	Libro from(LibroCSV libroCSV);	
+
+  @Mapping(ignore = true, target = "listaGeneros")
+  Libro from(
+      LibroCSV libroCSV);
+
 }
