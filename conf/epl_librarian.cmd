453718e26fc92ab510d5e8711e07b01090f80008
Verdoso <Verdoso@users.noreply.github.com>
2020-07-30 17:49:21 +0200

    Mejoramos el comando de lanzamiento de Windows

--- conf/epl_librarian.cmd
+++ conf/epl_librarian.cmd
@@ -1,2 +1,17 @@
+@ECHO OFF
+chcp 1250 > NUL
+
+ECHO Comprobando instalación de Java...
+where /q java || ECHO No hemos podido detectar java instalado en su sistema. No se puede ejecutar el programa. && EXIT /B
+ECHO Java detectado!
+
+ECHO Comprobando versión de Java...
+for /f tokens^=2-5^ delims^=.-_^" %%j in ('java -fullversion 2^>^&1') do set "jver=%%j%%k%%l%%m"
+if %jver% LSS 180000 ECHO La versión minima de Java soportada es la 1.8. No se puede ejecutar el programa. && EXIT /B
+ECHO La versión de Java es suficiente!
+
+ECHO Ejecutando...
+@ECHO ON
+
 java -jar epl_librarian-${project.version}.jar
 pause
