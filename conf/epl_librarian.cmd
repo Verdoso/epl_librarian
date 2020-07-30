c71345f0e6633183f6b41fbaf53e1d22b001cf11
Verdoso <Verdoso@users.noreply.github.com>
2020-07-30 18:36:38 +0200

    Pausamos antes de salir, para dejar ver el mensaje de error

--- conf/epl_librarian.cmd
+++ conf/epl_librarian.cmd
@@ -1,17 +1,17 @@
 @ECHO OFF
-chcp 1250 > NUL
+CHCP 1250 > NUL
 
 ECHO Comprobando instalación de Java...
-where /q java || ECHO No hemos podido detectar java instalado en su sistema. No se puede ejecutar el programa. && EXIT /B
+where /q java || ECHO No hemos podido detectar java instalado en su sistema. No se puede ejecutar el programa. && PAUSE && EXIT /B
 ECHO Java detectado!
 
 ECHO Comprobando versión de Java...
 for /f tokens^=2-5^ delims^=.-_^" %%j in ('java -fullversion 2^>^&1') do set "jver=%%j%%k%%l%%m"
-if %jver% LSS 180000 ECHO La versión minima de Java soportada es la 1.8. No se puede ejecutar el programa. && EXIT /B
+if %jver% LSS 180000 ECHO La versión minima de Java soportada es la 1.8. No se puede ejecutar el programa. && PAUSE && EXIT /B
 ECHO La versión de Java es suficiente!
 
 ECHO Ejecutando...
 @ECHO ON
 
 java -jar epl_librarian-${project.version}.jar
-pause
+PAUSE
