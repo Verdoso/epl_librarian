413b7d81665c6f8e2b930436d2c2d031ec81b063
verdoso <Verdoso@users.noreply.github.com>
2022-10-21 18:13:43 +0200

    Ignoramos explicitamente las propiedades de ejecución

--- src/main/java/org/greeneyed/epl/librarian/services/MapperService.java
+++ src/main/java/org/greeneyed/epl/librarian/services/MapperService.java
@@ -12,6 +12,10 @@ public interface MapperService {
   @Mapping(ignore = true, target = "inCalibre")
   @Mapping(ignore = true, target = "listaAutores")
   @Mapping(ignore = true, target = "listaMagnetIds")
+  @Mapping(ignore = true, target = "descartado")
+  @Mapping(ignore = true, target = "autorFavorito")
+  @Mapping(ignore = true, target = "generoFavorito")
+  @Mapping(ignore = true, target = "idiomaFavorito")  
   Libro from(LibroCSV libroCSV);
 
 }
