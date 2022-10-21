71527a35786dd9beea9d8a5ea4ead4cc1e65d877
verdoso <Verdoso@users.noreply.github.com>
2022-10-21 18:05:19 +0200

    Mejoramos el script Linux de arranque

--- conf/epl_librarian.cmd
+++ conf/epl_librarian.cmd
@@ -3,7 +3,7 @@ CHCP 1250 > NUL
 
 ECHO Comprobando instalación de Java...
 where /q java || ECHO No hemos podido detectar java instalado en su sistema. No se puede ejecutar el programa. && PAUSE && EXIT /B
-ECHO Java detectado!
+ECHO ¡Java detectado!
 
 ECHO Comprobando Java instalado...
 for /f "tokens=3" %%g in ('java -version 2^>^&1 ^| findstr /i "version"') do set "JAVAVER=%%g"
--- conf/epl_librarian.sh
+++ conf/epl_librarian.sh
@@ -1,1 +1,23 @@
-java -Xms1g -Xmx1g -jar epl_librarian-@project.version@.jar
+#!/bin/bash
+
+if type -p javas > /dev/null
+then
+    echo "¡Java detectado!"
+    _java=java
+elif  [ ! -z ${JAVA_HOME+x} ]  && [ -f "$JAVA_HOME/bin/java" ] 
+then
+    echo "¡Java detectado por JAVA_HOME!"     
+    _java="$JAVA_HOME/bin/java"
+else
+    echo "No hemos podido detectar java instalado en su sistema. No se puede ejecutar el programa."
+fi
+
+JAVA_VERSION=$($_java -version 2>&1 | sed -n ';s/.* version "\(.*\)\.\(.*\)\..*".*/\1\2/p;')
+
+if [[ "$JAVA_VERSION" -ge 170 ]]; then
+  echo "¡Versión de Java compatible!"
+  $_java -Xms1g -Xmx1g -jar epl_librarian-@project.version@.jar
+else         
+    echo "La versión minima de Java soportada es la 17. No se puede ejecutar el programa."
+fi
+
