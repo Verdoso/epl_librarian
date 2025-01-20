0d3ff569b6495b884401212a7e778c2069a963fc
scidlj <scidlj@MSI10>
2025-01-20 17:36:57 +0100

    Arreglamos los test para las preferencias ahora con modo docker

--- src/test/java/org/greeneyed/epl/librarian/services/PreferencesServiceTest.java
+++ src/test/java/org/greeneyed/epl/librarian/services/PreferencesServiceTest.java
@@ -3,24 +3,48 @@ package org.greeneyed.epl.librarian.services;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.startsWith;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.when;
 
 import java.io.File;
+import java.io.IOException;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.Profiles;
 
 public class PreferencesServiceTest {
 
-  private PreferencesService preferencesService = new PreferencesService();
+  @InjectMocks
+  private PreferencesService preferencesService;
+
+  @Mock
+  Environment environment;
+
+  @BeforeEach
+  public void setUp() throws Exception {
+    MockitoAnnotations.openMocks(this);
+    preferencesService = new PreferencesService();
+    preferencesService.setEnvironment(environment);
+    Mockito.reset(environment);
+  }
 
   @Test
-  public void testPreferencesFileLocation() {
-    File userHomepreferencesFile = PreferencesService.findPreferencesFile(false);
+  public void testPreferencesFileLocation() throws IOException {
+    when(environment.acceptsProfiles(any(Profiles.class))).thenReturn(Boolean.FALSE);
+    File userHomepreferencesFile = PreferencesService.findPreferencesFile(false, environment);
     File userHome = new File(System.getProperty("user.home"));
 
     assertThat("El fichero de preferencias no se creará en el $home del usuario.", userHomepreferencesFile.getAbsolutePath(),
         startsWith(userHome.getAbsolutePath()));
 
-    File currentDirPreferencesFile = PreferencesService.findPreferencesFile(true);
+    File currentDirPreferencesFile = PreferencesService.findPreferencesFile(true, environment);
     File currentDir = new File(System.getProperty("user.dir"));
     assertThat("El fichero de preferencias no se creará en el directorio actual.", currentDirPreferencesFile.getAbsolutePath(),
         startsWith(currentDir.getAbsolutePath()));
